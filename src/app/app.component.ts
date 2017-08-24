32addef158eb4cbcd20973d07c433c81e23620fd
Nic Wilson <nicwilson@gmail.com>
2017-08-24 16:46:55 -0500

    ToH Tutorial Step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -12,11 +14,6 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
-export class Hero {
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'my-app',
@@ -79,14 +76,7 @@ export class Hero {
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-  </div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
 `,
 })
 export class AppComponent  {
